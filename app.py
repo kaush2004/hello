8d9e2ede92c3ba15a9e96373c7943a8057c578cf
Kaushal <kaushalkumar_co21a4_57@dtu.ac.in>
2025-07-18 17:35:06 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+# print('hello world')
+# from math import *
+# print(sqrt(36))
+
+# name = input('Input your name : ')
+# age = int(input('Enter your age : '))
+# print(name, 'is',age,'years old')
+
+# List in python : Collect of diff data types
+# countries = ['UK', 'China', 'US', 'India']
+# countries[0] = 'UP'
+# print(countries)
+
+# Touples
+# you can't change  values in touples
+# three_numbers = (1,2,3,4)
+# print(type(three_numbers)
+
+# functions in python
+# def greetings_function(name):
+#     print('Hello', name)
+
+# if elif else statements 
+
+# Dictionaries : Objects in javascript and maps in C++
+# my_dict = {
+#     'name' : 'Kaushal',
+#     'nationality' : 'India',
+#     'Qualification' : 'College',
+#     'age' : '21'
+# }
+
+# while loops
+coun_file = open('hello.txt', 'r')
+print(coun_file.read())
+coun_file.close()
